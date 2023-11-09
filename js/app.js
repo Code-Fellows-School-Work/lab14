fb9a6f61e6b9d478ae26b8d988ece7c20b751ac9
Errol Vidad <errolvidad@gmail.com>
2023-11-09 21:57:28 +0000

    updated save and load functions

--- js/app.js
+++ js/app.js
@@ -29,17 +29,38 @@ AppState.prototype.saveToLocalStorage = function () {
 }
 // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
 AppState.prototype.loadItems = function () {
+  let storedData = localStorage.getItem('savedData');
+  console.log(storedData);
+  if (storedData) {
+    let parseData = JSON.parse(storedData);
+    console.log(parseData);
+    for (let i = 0; i < parseData.AppState.length; i++) {
+      let getData = parseData.AppState[i];
+      new Product(getData.name, getData.source, getData.timesClicked, getData.timesShown)
+    }
+  } else AppState.prototype.instantiateProducts = function () {
 
+    const productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
   
-
+  // loop to find sweep and modify file extension from jpg to png
+  
+    for (let i = 0; i < productNames.length; i++) {
+      if (productNames[i] === 'sweep') {
+        this.allProducts.push(new Product(productNames[i], 'png'))
+      } else {
+        this.allProducts.push(new Product(productNames[i]))
+      }
+    }
+  
+  }
   this.instantiateProducts();
 
 }
 
 
-function Product(name, fileExtension = 'jpg') {
+function Product(name, fileExtension = 'jpg', timesClicked = 0, timesShown = 0) {
   this.name = name;
   this.source = `assets/${name}.${fileExtension}`;
-  this.timesClicked = 0;
-  this.timesShown = 0;
+  this.timesClicked = timesClicked;
+  this.timesShown = timesShown;
 }
